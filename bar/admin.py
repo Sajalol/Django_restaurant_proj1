1d1b1e330c6e8d9975892c3708885d9e68c56735
Stian Lystad <Stianlystad_1@hotmail.com>
2022-07-25 12:08:10 +0000

    add models.py structure and admin.py structure

--- bar/admin.py
+++ bar/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Restaurant, Customer, Table, Reservation, Menu
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+admin.site.register(Customer)
+admin.site.register(Table)
+admin.site.register(Reservation)
+
+@admin.register(Menu)
+class MenuAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('Menu')
--- bar/models.py
+++ bar/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+class Restaurant(models.Model):
+    name = models.CharField(max_length=120)
+
+class Customer(models.Model):
+    email = models.EmailField()
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+
+class Table(models.Model):
+    seats = models.IntegerField()
+    min_people = models.IntegerField()
+    max_people = models.IntegerField()
+
+class Reservation(models.Model):
+    table = models.ForeignKey('Table', on_delete=models.CASCADE)
+    party = models.ForeignKey('Customer', on_delete=models.CASCADE)
+    spot = models.DateField()
+
+class Menu(models.Model):
+    nameOfFood = models.CharField(max_length=30)
+    meat = models.CharField(max_length=30)
+    allergy = models.CharField(max_length=30)
+    food_details = models.CharField(max_length=150)
