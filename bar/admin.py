45f257d26a0e34f92718315ce140b0ebbdbbe725
Stian Lystad <Stianlystad_1@hotmail.com>
2022-07-26 08:58:08 +0000

    add more details to models.py and admin.py

--- bar/admin.py
+++ bar/admin.py
@@ -3,11 +3,25 @@ from .models import Restaurant, Customer, Table, Reservation, Menu
 from django_summernote.admin import SummernoteModelAdmin
 
 
-admin.site.register(Customer)
-admin.site.register(Table)
-admin.site.register(Reservation)
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+     list_display = ('email', 'first_name', 'last_name')
+
+@admin.register(Table)
+class TableAdmin(admin.ModelAdmin):
+    list_display = ('seats', 'min_people', 'max_people')
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    list_filter = ('approved', 'table')
+    list_display = ('party', 'table')
+    actions = ['approve_Reservation']
+
+    def approve_Reservation(self, request, queryset):
+        queryset.update(approved=True)
 
 @admin.register(Menu)
 class MenuAdmin(SummernoteModelAdmin):
+    list_display = ('nameOfFood', 'meat', 'allergy', 'food_details', 'price')
 
     summernote_fields = ('food_details')
--- bar/models.py
+++ bar/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
+
 class Restaurant(models.Model):
     name = models.CharField(max_length=120)
 
@@ -19,6 +20,7 @@ class Reservation(models.Model):
     table = models.ForeignKey('Table', on_delete=models.CASCADE)
     party = models.ForeignKey('Customer', on_delete=models.CASCADE)
     spot = models.DateField()
+    approved = models.BooleanField(default=False)
 
 class Menu(models.Model):
     nameOfFood = models.CharField(max_length=30)
