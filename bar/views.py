14e57f88be4f19e5890c7fe25d6bca70c11f1e3d
Stian Lystad <Stianlystad_1@hotmail.com>
2022-07-27 11:59:01 +0000

    add more to basic templates and signup templates

--- bar/views.py
+++ bar/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import Restaurant, Menu, Reservation
+from .forms import ReservationForm
 
 class RestaurantList(generic.ListView):
     model = Restaurant
@@ -13,6 +14,3 @@ class MenuList(generic.ListView):
 class ReservationList(generic.ListView):
     model = Reservation
     template_name = 'reservation.html'
-
-
-
--- restaurant/urls.py
+++ restaurant/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path('', include('bar.urls'), name='bar_urls'),
+    path('accounts/', include('allauth.urls')),
 ]
