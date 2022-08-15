4723aaa444912ebfd087a90a971821bb4892871a
Stian Lystad <Stianlystad_1@hotmail.com>
2022-08-15 23:02:16 +0000

    testing forms and views with reservation

--- bar/views.py
+++ bar/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
-from .models import Restaurant, Menu, Reservation
+from .models import Restaurant, Menu, Reservation, Customer, Table
 from .forms import ReservationForm
 
 class RestaurantList(generic.ListView):
@@ -14,3 +14,15 @@ class MenuList(generic.ListView):
 class ReservationList(generic.ListView):
     model = Reservation
     template_name = 'reservation.html'
+
+def reservations(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        comment = request.POST.get('comment')
+
+        reservations = Table(name=name, email=email, comment=comment)
+
+        reservations.save()
+
+    return render(request, 'reservation.html')
