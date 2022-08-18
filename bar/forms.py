671297820e4beb10003de0873b522c5851ac8206
Stian Lystad <Stianlystad_1@hotmail.com>
2022-08-18 19:11:07 +0000

    added text to reservation.html and added time + date

--- bar/forms.py
+++ bar/forms.py
@@ -1,8 +1,18 @@
 from django import forms
 from django.forms import fields
-from .models import Reservation
+from .models import Reservation, Table
+
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
+class TimeInput(forms.TimeInput):
+    input_type = 'time'
 
 class ReserveTableForm(forms.ModelForm):
     class Meta:
         model = Reservation
         fields = ('name', 'email', 'phone', 'number_of_guests', 'date', 'time',)
+        widgets = {
+            'date':DateInput(),
+            'time':TimeInput(),
+        }
